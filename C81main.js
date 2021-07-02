403c9ad1519c9c2e2ee0008780d7d9e0640115e3
aaronc1028 <81634761+aaronc1028@users.noreply.github.com>
2021-07-02 09:57:22 -0400

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -10,11 +10,38 @@ function drawCircle(e) {
     circle(mouseX, mouseY)
 }
 function circle(x, y) {
+    
     ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 2;
-    ctx.arc(x, y, 40, 0, 2 * Math.PI);
+    ctx.strokeStyle =red;
+    ctx.lineWidth = 1;
+    ctx.arc(150, 143, 430, 200);
     ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle :green ;
+    ctx.lineWidth = 2;
+    ctx.arc(150, 143, 430, 200);
+    ctx.stroke();
+   
+    ctx.beginPath();
+    ctx.strokeStyle =grey;
+    ctx.lineWidth = 3;
+    ctx.arc(150, 143, 430, 200);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle =yellow;
+    ctx.lineWidth = 4;
+    ctx.arc(150, 143, 430, 200);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle =blue;
+    ctx.lineWidth = 5;
+    ctx.arc(100, 40, 0, 2*Math.PI);
+    ctx.stroke();
+   
+   
+   
+ 
 }
 function clearcanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
