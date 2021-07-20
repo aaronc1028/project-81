191d9c7e7797e19e02c4dd791e3d8448bff53a6f
aaronc1028 <81634761+aaronc1028@users.noreply.github.com>
2021-07-20 19:56:20 -0400

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -2,47 +2,50 @@ var canvas = document.getElementById("circleCanvas");
 var ctx = canvas.getContext("2d");
 var color = "red";
 
-canvas.addEventListener("mousedown", drawCircle)
-function drawCircle(e) {
-    color = document.getElementById("textInput").value;
-    mouseX = e.clientX - canvas.offsetLeft;
-    mouseY = e.clientY - canvas.offsetTop;
-    circle(mouseX, mouseY)
-}
-function circle(x, y) {
+
+
+
     
     ctx.beginPath();
-    ctx.strokeStyle =red;
+    ctx.strokeStyle ="black";
     ctx.lineWidth = 1;
-    ctx.arc(150, 143, 430, 200);
+    ctx.rect(150, 143, 430, 200);
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.strokeStyle :green ;
+    ctx.strokeStyle ="blue" ;
     ctx.lineWidth = 2;
-    ctx.arc(150, 143, 430, 200);
+    ctx.arc(250,210,40,0,2*Math.PI);
     ctx.stroke();
    
     ctx.beginPath();
-    ctx.strokeStyle =grey;
+    ctx.strokeStyle ="black";
     ctx.lineWidth = 3;
-    ctx.arc(150, 143, 430, 200);
+    ctx.arc(350,210,40,0,2*Math.PI);
     ctx.stroke();
+
     ctx.beginPath();
-    ctx.strokeStyle =yellow;
+    ctx.strokeStyle ="red";
     ctx.lineWidth = 4;
-    ctx.arc(150, 143, 430, 200);
+    ctx.arc(450, 210, 40, 0,2*Math.PI);
     ctx.stroke();
+
     ctx.beginPath();
-    ctx.strokeStyle =blue;
+    ctx.strokeStyle ="gold";
     ctx.lineWidth = 5;
-    ctx.arc(100, 40, 0, 2*Math.PI);
+    ctx.arc(300, 250, 40,0, 2*Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle ="green";
+    ctx.lineWidth = 5;
+    ctx.arc(400,250, 40, 0, 2*Math.PI);
     ctx.stroke();
    
    
    
  
-}
+
 function clearcanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
